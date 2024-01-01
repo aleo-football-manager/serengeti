93c46fa65f8142dfc3c34325a9e6d89889bcc484
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-01-01 04:00:09 +0100

    updated players

---
+++ scripts/import_players.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import subprocess
+from dotenv import load_dotenv
+import os
+
+# Load the environment variables from .env file
+load_dotenv()
+
+# Your private key (loaded from .env file)
+private_key = os.getenv('PRIVATE_KEY_MAIN')
+
+# Load the CSV file
+csv_file_path = '../files/players_final_u8.csv'
+df = pd.read_csv(csv_file_path)
+
+
+# Loop through each row in the dataframe
+for index, row in df.iterrows():
+    # Constructing the CLI command
+    cli_command = (
+        f"snarkos developer execute \"football_game_v005.aleo\" \"add_player\" "
+        f"\"{{player_id: {row['player_uid']}u8,team_id: {row['team_id']}u8,position: {row['position']}field,"
+        f"attack: {row['attack']}u8,defense: {row['defense']}u8,speed: {row['speed']}u8,"
+        f"power: {row['power']}u8,stamina: {row['stamina']}u8,technique: {row['technique']}u8,"
+        f"goalkeeping: {row['goalkeeping']}u8}}\" --private-key \"{private_key}\" "
+        f"--query \"https://api.explorer.aleo.org/v1\" --broadcast \"https://api.explorer.aleo.org/v1/testnet3/transaction/broadcast\" "
+        f"--priority-fee 1000000"
+    )
+
+    # Execute the CLI command
+    subprocess.run(cli_command, shell=True)
+
+print("All players have been processed.")
