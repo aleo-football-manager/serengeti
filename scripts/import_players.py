74c4a2f4cc6c42e023dd0edd3d9b068ad10ac519
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-03-15 22:20:38 +0800

    update costs

--- scripts/import_players.py
+++ scripts/import_players.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 
 # Run this command to execute script (from scripts dir):
-# python3 ../scripts/test_formation.py
+# python3 ../scripts/import_players.py
 
 # Load the environment variables from .env file
 load_dotenv()
@@ -13,7 +13,7 @@ load_dotenv()
 private_key = os.getenv('PRIVATE_KEY')
 
 # Load the CSV file
-csv_file_path = '../files/players_final_u8_short.csv'
+csv_file_path = '../files/players_final_u8_100.csv'
 df = pd.read_csv(csv_file_path)
 
 
@@ -21,11 +21,11 @@ df = pd.read_csv(csv_file_path)
 for index, row in df.iterrows():
     # Constructing the CLI command
     cli_command = (
-        f"snarkos developer execute \"formation_tests_v002.aleo\" \"add_player\" "
-        f"\"{{player_id: {row['player_uid']}u8,team_id: {row['team_id']}u8,position: {row['position']}field,"
-        f"attack: {row['attack']}u8,defense: {row['defense']}u8,speed: {row['speed']}u8,"
-        f"power: {row['power']}u8,stamina: {row['stamina']}u8,technique: {row['technique']}u8,"
-        f"goalkeeping: {row['goalkeeping']}u8}}\" --private-key \"{private_key}\" "
+        f"snarkos developer execute \"cost_tests_calc_v006.aleo\" \"add_player\" "
+        f"\"{{player_id: {row['player_uid']}field,team_id: {row['team_id']}field,position: {row['position']}field,"
+        f"attack: {row['attack']}u128,defense: {row['defense']}u128,speed: {row['speed']}u128,"
+        f"power: {row['power']}u128,stamina: {row['stamina']}u128,technique: {row['technique']}u128,"
+        f"goalkeeping: {row['goalkeeping']}u128}}\" --private-key \"{private_key}\" "
         f"--query \"https://node.puzzle.online\" --broadcast \"https://node.puzzle.online/testnet3/transaction/broadcast\" "
         f"--priority-fee 0"
     )
